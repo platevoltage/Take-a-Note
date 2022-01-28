ba13dcb6ad338b052d09ada01bad3926a95c3e6b
antieatingactivist <antieatingactivist@gmail.com>
2022-01-27 22:35:22 -0800

    added post and get listeners for notes

--- server.js
+++ server.js
@@ -6,14 +6,28 @@ const PORT = 3001;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
 
+  res.json(`${req.method} request received`);
+
+  console.info(`${req.method} request received`);
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+  res.json(`${req.method} request received`);
+  
+  console.info(`${req.method} request received`);
+
+});
 
 
 
