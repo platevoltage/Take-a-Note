0502970c62a2343649b2602c379aa2271fbf851f
antieatingactivist <antieatingactivist@gmail.com>
2022-01-27 22:18:04 -0800

    added paths to html files in public

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
